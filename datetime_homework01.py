dec7a302db80ebf95619c94339a8ef9dc8a89773
unknown <shamsiyevalibek90@gmail.com>
2025-04-30 19:57:28 +0300

    islombek

--- datetime_homework01.py
+++ datetime_homework01.py
@@ -1,2 +1,5 @@
 #Task 1:
 #Write a Python program that displays the current date and time.
+import datetime
+current_datetime = datetime.datetime.now()
+print("Joriy sana va vaqt:", current_datetime)
