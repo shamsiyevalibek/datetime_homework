4f43f91363218ccd08bb5879e8de7d004d8ffaee
unknown <shamsiyevalibek90@gmail.com>
2025-04-30 20:15:46 +0300

    qanday

--- datetime_homework05.py
+++ datetime_homework05.py
@@ -1,2 +1,7 @@
 #Task 5:
 #Write a Python program that prompts the user to enter a specific date in the format "dd-mm-yyyy" and determines the day of the week on which that date falls.
+from datetime import datetime
+user_input = input("Iltimos, sana kiriting (dd-mm-yyyy formatida): ")
+user_date = datetime.strptime(user_input, "%d-%m-%Y")
+day_of_week = user_date.strftime("%A")
+print(f"Kiritilgan sana {day_of_week} kuniga to'g'ri keladi.")
